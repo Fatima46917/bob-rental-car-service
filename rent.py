61ca78a8064d82b6162f1e70e62f2b489633b8ec
fatima sharaf eddine <fatumash44@gmail.com>
2025-02-28 20:26:34 +0200

    rental price per day a private attribute

--- rent.py
+++ rent.py
@@ -12,16 +12,31 @@ class vehicles:
     print (f"year:{self.year}")
     print (f"rental_price_per_day:{self.rental_price_per_day}")
 
+
   def rental_cost_per_days (self, number_of_days):
     self.number_of_days = number_of_days
     print(self.number_of_days * self.rental_price_per_day)
 
 class car (vehicles) :
+  def __init__(self):
+    self.__rental_price_per_day = 50 
+
+  def getrental_price_per_day (self):
+    return self.__rental_price_per_day
+
   def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
     self.seating_capacity = seating_capacity
     super().__init__(brand, model, year, rental_price_per_day)
 
 class bike (vehicles):
+  def __init__(self):
+    self.__rental_price_per_day = 30
+
+  def getrental_price_per_day(self) :
+    return self.__rental_price_per_day
+  
+
   def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
     self.engine_capacity = engine_capacity
     super().__init__(brand, model, year, rental_price_per_day)
+
