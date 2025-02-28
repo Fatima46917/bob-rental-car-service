72fdeaa034dfb76a82204585955fc65c0c95bb58
fatima sharaf eddine <fatumash44@gmail.com>
2025-02-28 19:38:59 +0200

    rental cost / days method

--- rent.py
+++ rent.py
@@ -11,3 +11,8 @@ class vehicles:
     print (f"model : {self.model}")
     print (f"year:{self.year}")
     print (f"rental_price_per_day:{self.rental_price_per_day}")
+
+  def rental_cost_per_days (self, number_of_days):
+    self.number_of_days = number_of_days
+    print(self.number_of_days * self.rental_price_per_day)
+    
