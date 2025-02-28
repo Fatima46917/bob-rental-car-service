edbbf84563f7850f402b6dd6dcd10e5d94d00a7e
fatima sharaf eddine <fatumash44@gmail.com>
2025-02-28 20:39:57 +0200

    rental price per day is private corrrection

--- rent.py
+++ rent.py
@@ -1,42 +1,29 @@
 vehicles = []
 class vehicles:
-  def __init__(self, brand, model, year, rental_price_per_day):
+  def __init__(self, brand, model, year, price):
     self.brand = brand
     self.model = model
     self.year = year
-    self.rental_price_per_day = rental_price_per_day
+    self.__rental_price_per_day = price
 
   def display(self):
     print (f"brand :{self.brand}")
     print (f"model : {self.model}")
     print (f"year:{self.year}")
-    print (f"rental_price_per_day:{self.rental_price_per_day}")
+    print (f"rental_price_per_day:{self.__rental_price_per_day}")
 
 
   def rental_cost_per_days (self, number_of_days):
     self.number_of_days = number_of_days
-    print(self.number_of_days * self.rental_price_per_day)
+    print(self.number_of_days * self.__rental_price_per_day)
 
 class car (vehicles) :
-  def __init__(self):
-    self.__rental_price_per_day = 50 
-
-  def getrental_price_per_day (self):
-    return self.__rental_price_per_day
-
-  def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
+  def __init__(self, brand, model, year, price, seating_capacity):
     self.seating_capacity = seating_capacity
-    super().__init__(brand, model, year, rental_price_per_day)
+    super().__init__(brand, model, year, price)
 
 class bike (vehicles):
-  def __init__(self):
-    self.__rental_price_per_day = 30
-
-  def getrental_price_per_day(self) :
-    return self.__rental_price_per_day
-  
-
-  def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+  def __init__(self, brand, model, year, price, engine_capacity):
     self.engine_capacity = engine_capacity
-    super().__init__(brand, model, year, rental_price_per_day)
+    super().__init__(brand, model, year, price)
 
