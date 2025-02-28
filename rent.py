a6cb79e2d6628f4306332653c8286fe753e748c4
fatima sharaf eddine <fatumash44@gmail.com>
2025-02-28 18:56:50 +0200

    vehicle class

--- rent.py
+++ rent.py
@@ -5,29 +5,3 @@ class vehicles:
     self.year = year
     self.rental_price_per_day = rental_price_per_day
 
-vehicle1 = vehicles("car", "Toyota Corolla", 2020, "50$/day")
-vehicle2 = vehicles("bike", "Yamaha R1", 2019, "30$/day")
-
-vehicle = {
-  "car" : "Toyota Corolla",
-  "year": "2020",
-  "seats": "5",
-  "rental_price_per_day" : "50$/day"
-}
-print(vehicle)
-
-bike = {
-  "Bike" : "Yamaha R1",
-  "year" : "2019",
-  "rental_price_per_day" : "30$/day",
-  "engine" : "998cc"
-}
-print(bike)
-
-
-
-
-
-
-
- 
